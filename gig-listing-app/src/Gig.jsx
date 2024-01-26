2d4bc4d91241017f42bfa643d785b168af8aad50
Sarah Davies <sarahdavies186@gmail.com>
2024-01-26 16:55:03 +0000

    updated with new project

---
+++ gig-listing-app/src/Gig.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Gig.css'
+
+function Gig() {
+  return (
+    <>
+    <h3 className='artist-name'>Cleo Sol</h3>
+    <img className="artist-image" src="https://i0.wp.com/pipermintblog.com/wp-content/uploads/2020/10/cleo-sol-3.jpg?resize=1038%2C576&ssl=1"/>
+    <p className='artist-text'>Your girl Clee, live at the Royal Albert Hall for one night only</p>
+    <p className='artist-text'>Sunday 18th February 2024 20:00</p>
+    <p className='artist-text'>LONDON</p>
+    </>
+  )
+}
+
+export default Gig;
---
+++ vite-project/src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div id="profile">
+      <h1 id="name">Quackie Makers</h1>
+
+      <p id="job">Makers' favourite rubber duck</p>
+      <p id="birthdate">2013</p>
+    </div>
+  );
+}
+
+export default Profile;
