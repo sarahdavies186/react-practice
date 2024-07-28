f63a139edd06796bb560fd2105bdffcf07f4d209
Sarah Davies <sarahdavies186@gmail.com>
2024-07-28 10:52:47 +0100

    react practice

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -4,14 +4,7 @@ const data = [
     title: "The Lord of the Rings",
     publicationDate: "1954-07-29",
     author: "J. R. R. Tolkien",
-    genres: [
-      "fantasy",
-      "high-fantasy",
-      "adventure",
-      "fiction",
-      "novels",
-      "literature",
-    ],
+    genres: ["fantasy", "high-fantasy", "adventure", "fiction", "novels", "literature"],
     hasMovieAdaptation: true,
     pages: 1216,
     translations: {
@@ -37,13 +30,7 @@ const data = [
     title: "The Cyberiad",
     publicationDate: "1965-01-01",
     author: "Stanislaw Lem",
-    genres: [
-      "science fiction",
-      "humor",
-      "speculative fiction",
-      "short stories",
-      "fantasy",
-    ],
+    genres: ["science fiction", "humor", "speculative fiction", "short stories", "fantasy"],
     hasMovieAdaptation: false,
     pages: 295,
     translations: {},
@@ -142,3 +129,127 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+/*
+
+//Destructing
+
+const book = getBook(3);
+// const title = book.title;
+// book
+
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+  book;
+
+//Destructuring with arrays
+console.log(genres);
+
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, secondaryGenre, otherGenres);
+
+const newGenres = [...genres, "epic fantasy"];
+console.log(newGenres)
+
+//adding a new property and overwriting an existing property - spread operator to be first
+const updatedBook = { ...book, moviePublicationDate: "2002-10-10", pages: 1020};
+console.log(updatedBook)
+
+// function getYear(str) {
+//   return str.split("-")[0];
+// }
+
+const getYear = str => str.split("-")[0];
+console.log(getYear(publicationDate))
+
+const summary = `The title of the book is ${title}, written by ${author}, published in the year ${getYear(publicationDate)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted to a movie.`;
+console.log(summary)
+
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pagesRange
+
+console.log(false && "Some string")
+console.log("some string" && "somestring")
+
+const spanishTranslation = book.translations.spanish || "NOT translated";
+spanishTranslation
+
+// const wrongData = book.reviews.librarything.reviewsCount || "no data"
+// wrongData
+
+// const count = book.reviews.librarything.reviewsCount ?? "no data";
+// count
+
+function getTotalReviewCount(book) {
+  const goodread = book.reviews?.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  return goodread + librarything;
+}
+
+console.log(getTotalReviewCount(book))
+
+*/
+
+/*
+const books = getBooks();
+books;
+
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+x;
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+}));
+essentialData;
+
+const longBooks = books.filter((book) => book.pages > 500).filter((book) => book.hasMovieAdaptation);
+longBooks;
+
+const adventureBooks = books.filter((book) => book.genres.includes("adventure")).map((book) => book.title);
+
+adventureBooks;
+
+const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
+pagesAllBooks;
+
+const arr = [3, 1, 2, 4];
+const sorted = arr.slice().sort((a, b) => a - b);
+sorted;
+arr;
+
+const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
+sortedByPages;
+
+// 1- Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry potter and chamber of secrets",
+  author: "J. K. Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2 - Delete a book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3= update a book object in the array
+const bookAfterUpdate = booksAfterDelete.map((book) => (book.id === 1 ? {...book, pages: 1210} : book));
+bookAfterUpdate
+*/
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+  async function getTodos() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data)
+  }
+
+  getTodos()
