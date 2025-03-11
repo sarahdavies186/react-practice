98d2a211059d13c9d3c564f8fa94a71e98c39969
Sarah Davies <sarahdavies186@gmail.com>
2025-03-11 13:53:24 +0000

    star rating component added

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -55,8 +55,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box element={<MovieList movies={movies} />} />
-
+        {/* <Box element={<MovieList movies={movies} />} />
         <Box
           element={
             <>
@@ -64,14 +63,14 @@ export default function App() {
               <WatchedMoviesList watched={watched} />
             </>
           }
-        />
-        {/* <Box>
+        /> */}
+        <Box>
           <MovieList movies={movies} />{" "}
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
-        </Box> */}
+        </Box>
       </Main>
     </>
   );
@@ -114,7 +113,7 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ element }) {
+function Box({ children }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -122,7 +121,7 @@ function Box({ element }) {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && element}
+      {isOpen && children}
     </div>
   );
 }
--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+
+import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return(
+    <div>
+      <StarRating maxRating={5} defaultRating={3} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} defaultRating={3}/>
+    <Test />
   </React.StrictMode>
 );
 
