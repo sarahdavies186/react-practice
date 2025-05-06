47b8efc185a2f870bac62aa862323c1078dee2e8
Sarah Davies <sarahdavies186@gmail.com>
2025-05-06 15:22:29 +0100

    added timer and completed project

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -8,6 +8,10 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishedScreen from "./FinishedScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -16,6 +20,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -25,7 +30,7 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
     case "start":
-      return { ...state, status: "active" };
+      return { ...state, status: "active", secondsRemaining: state.questions.length * SECS_PER_QUESTION };
     case "newAnswer":
       const question = state.questions.at(state.index);
       return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points };
@@ -35,12 +40,14 @@ function reducer(state, action) {
       return { ...state, status: "finished" };
     case "restart":
       return { ...initialState, questions: state.questions, status: "ready"};
+    case "tick": 
+      return {...state, secondsRemaining: state.secondsRemaining - 1, status: state.secondsRemaining === 0 ? "finished" : state.status}
     default:
       throw new Error("Action unknown");
   }
 }
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, secondsRemaining}, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0);
@@ -67,7 +74,10 @@ export default function App() {
         {status === "active" && (
           <>
             <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} />
-            <Question question={questions[index]} dispatch={dispatch} answer={answer} /> <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
+            <Question question={questions[index]} dispatch={dispatch} answer={answer} /> 
+            <Footer />
+            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining}/>
+            <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
           </>
         )}
         {status === "finished" && <FinishedScreen points={points} maxPossiblePoints={maxPossiblePoints} dispatch={dispatch} />}
